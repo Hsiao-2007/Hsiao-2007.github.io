af24caa23a790705d881c0a1b0508c82cf114f4f
Hsiao-2007 <147453729+Hsiao-2007@users.noreply.github.com>
2024-07-05 17:30:07 -0400

    Updated functions.js to contain notes about future endavours

--- functions.js
+++ functions.js
@@ -1,10 +1,10 @@
 function checkInput() {
     var element = document.getElementById("terminal-text");
     var promptText = document.getElementById("input-command");
-    if (promptText.value == "cat /home/hsiao/.info" || promptText.value == "cat /../hsiao/.info") { //Check if command entered is reading the .info file in the home directory
-        element.innerHTML += "<br>My bio..."
-    } 
-    else {
-        element.innerHTML += "<br> ERROR! Please enter in cat the contents on the .info file in /home/hsiao"
-    }
+    if (promptText.value == "cat /home/hsiao/.info" || promptText.value == "cat /../hsiao/.info") element.innerHTML += "<br>My bio..." /*
+    1. Add slowdown feature to imitate "typing"
+    2. Make it read the contents of a local .info file
+    3. Fix it to actually use a local shell */
+    else if (promptText.value == "help") element.innerHTML += "Use the cat command to read the contents of /home/hsiao/.info";
+    else element.innerHTML += "<br> ERROR! Please enter in cat the contents on the .info file in /home/hsiao"
 }
